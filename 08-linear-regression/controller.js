5bfc51b9a24df7e38f381985ae3658dfbd96ba1d
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-03-25 04:13:58 -0600

    Update controller.js

--- 08-linear-regression/controller.js
+++ 08-linear-regression/controller.js
@@ -1,15 +1,19 @@
 
 
 (async () => {
-    const { LinearRegression, trainTestSplit } = await import("https://cdn.jsdelivr.net/gh/mlearnjs/mlearnjs@main/dist/mlearn.mjs");
-
+    const { 
+        LinearRegression, 
+        trainTestSplit, 
+        joinArrays
+    } = await import("https://cdn.jsdelivr.net/gh/mlearnjs/mlearnjs@main/dist/mlearn.mjs");
+      
     var xTrain = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]];
     var yTrain = [1, 4, 1, 5, 3, 7, 2, 7, 4, 9];
   
     const model = new LinearRegression();
     model.fit(xTrain, yTrain)
     yPredict = model.predict(xTrain)
-    
+
     document.getElementById("log").innerHTML+='<br>X Train:   '+xTrain+'<br>Y Train:   '+yTrain+'<br>Y Predict: '+yPredict
     var a = joinArrays('x',xTrain,'yTrain',yTrain,'yPredict',yPredict)
 
