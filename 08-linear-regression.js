ca4c7faa3ba40d13a5f183886401a2fc6de502f2
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-04-01 23:08:41 -0600

    Update 08-linear-regression.js

--- 08-linear-regression.js
+++ 08-linear-regression.js
@@ -1,12 +1,13 @@
-const { LinearRegression, joinArrays } = await import('https://cdn.jsdelivr.net/gh/mlearnjs/mlearnjs.github.io@main/dist/mlearn.mjs?v=' + new Date().getTime());
-
-const X = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const y = [1, 4, 1, 5, 3, 7, 2, 7, 4, 9];
-
 async function fit_predict_draw() {
+
+    const { LinearRegression, joinArrays } = await import('https://cdn.jsdelivr.net/gh/mlearnjs/mlearnjs.github.io@main/dist/mlearn.mjs?v=' + new Date().getTime());
+
     const myLinearRegression = await LinearRegression(); 
     const model = new myLinearRegression();
-    
+
+    const X = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const y = [1, 4, 1, 5, 3, 7, 2, 7, 4, 9];
+
     model.fit(X, y);
 
     yPredict = model.predict(X)
